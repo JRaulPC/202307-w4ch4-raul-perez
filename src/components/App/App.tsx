f994a1570e8b7e0368989a4fcf32364d11fbcc5f
Raúl Pérez <jrpcassone@gmail.com>
2023-08-03 21:30:56 +0200

    Add api response types and css

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,12 @@
 import SlothsForm from "../SlothsForm/SlothsForm";
 
 const App = (): React.ReactElement => {
-  return <SlothsForm />;
+  return (
+    <div>
+      <h1 className="main-title">Slot API Pusher</h1>
+      <SlothsForm />;
+    </div>
+  );
 };
 
 export default App;
